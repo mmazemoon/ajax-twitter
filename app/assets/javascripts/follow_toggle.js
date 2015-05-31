d3f8737ce12d19bac5a78978241c394e25da44d5
Mark Azemoon <mmazemoon@gmail.com>
2015-05-31 12:26:27 -0700

    Extract user-id and initial-follow-state from button and save in ivars

--- app/assets/javascripts/follow_toggle.js
+++ app/assets/javascripts/follow_toggle.js
@@ -1,9 +1,12 @@
+// constructor
 $.FollowToggle = function (el) {
-
+  this.$el = $(el);
+  this.userId = this.$el.data("user-id");
+  this.followState = this.$el.data("initial-follow-state");
 };
 
 $.FollowToggle.prototype.method1 = function() {
-
+  
 };
 
 $.fn.FollowToggle = function (){
