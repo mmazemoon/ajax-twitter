c28b92437d521d751892b446bd2cfc2b4e0fbce5
Mark Azemoon <mmazemoon@gmail.com>
2015-05-31 18:54:40 -0700

    Add input even listener for input tag

--- app/assets/javascripts/users_search.js
+++ app/assets/javascripts/users_search.js
@@ -2,4 +2,6 @@ $.UsersSearch = function (el) {
   this.$el = $(el);
   this.$input = this.$el.find("input[name=username]");
   this.$ul = this.$el.find(".users");
+
+  this.$input.on("input", this.handleInput.bind(this));
 };
